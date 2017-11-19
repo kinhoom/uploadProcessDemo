dbe8acb486b7f09b69101ce30962d97c821bfc55
alex kinhoom <alex kinhoom>
2017-11-19 15:33:57 +0800

    add processText

--- apc.php
+++ apc.php
@@ -1,7 +1,8 @@
 <?php
 $res=apc_fetch('upload_1243');
-apc_delete('upload_1243');
+// apc_delete('upload_1243');
 if($res[total])
- 	$total=($res[current]/$res[total]);
+ 	$total=round($res[current]*100/$res[total]);
  echo $total;
-
+ if($total==0)
+ 	echo 3333;
